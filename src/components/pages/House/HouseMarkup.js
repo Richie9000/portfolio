eeeb4949f1954d1dc9e75173c92e57f0741943fc
Ricardo <rr155914@gmail.com>
2022-06-05 17:23:19 -0500

    Add house details

--- src/components/pages/House/HouseMarkup.js
+++ src/components/pages/House/HouseMarkup.js
@@ -6,17 +6,25 @@ import Mdvr from "./Mdvr"
 
 const HouseMarkup = () => {
   return (
-    <div div className="Container" style={{ width:"100%", height:"100vh" } }>
+    <div div className="Container" style={{ width:"100%", height:"80vh" } }>
         <Canvas camera={{ fov: 70, position: [ 28, 0, -20 ]}}>
      <Suspense fallback={null}>
-       <ambientLight intensity={3}/>
+       <ambientLight intensity={5}/>
+       <spotLight position={[70, 41, 60]} intensity={10} angle={0.29} penumbra={1} shadow-mapSize={[512, 512]} castShadow />
        <Mdvr position={[15, -5, -5]} />
 
        <OrbitControls />
     </Suspense>
 
      </Canvas>
-       
+    <footer style={{margin: "15px"}} >
+      If you wanna see the live action of the scene, you can visit...
+      <a href="https://housedecentraland.herokuapp.com?realm=localhost-stub&renderer-branch=master">3d World!</a>
+      <div>
+        (Loading the 3d world may take a few moments) If you wanna know more about Decentraland visit....
+      <a href="https://decentraland.org/">Decentraland</a>
+      </div>
+    </footer>
     </div>
   )
 }
