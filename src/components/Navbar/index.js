8cf5387522be8452629a5782fa29bd66587181f6
Ricardo <rr155914@gmail.com>
2022-06-08 20:28:26 -0500

    add basic pop up

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,19 +2,26 @@ import React, { useRef, useState } from 'react'
 import { Link } from 'react-router-dom';
 import { Breakpoint } from 'react-socks';
 
-const Hamb =() =>{
- 
 
+
+const Hamb =() =>{
+ const closed = useRef();
+  function CloseNav (){
+    closed.current.style.display = "none"
+  }
+  
   return(
-    <div className="containerS" style={{ height: "100%", width: "100%", backgroundColor:"black", zIndex: "2", position: "absolute", opacity: "95%"}}>
-   <ul style={{margin: "15px"}}  >
+    <div ref={closed} className="containerS" style={{ top: 0, height: "100%", width: "100%", backgroundColor:"black", zIndex: "2", position: "absolute", opacity: "95%"}}>
+      <ul style={{margin: "15px"}}  >
    <Link className="nav-list" to="/" style={{margin: "15px"}}  >Home</Link>
    <Link className="nav-list" to="/watch" style={{  margin: "15px"}} >Watch</Link>
    <Link className="nav-list" to="/model" style={{margin: "15px"}} >Shoe Model</Link>
    <Link className="nav-list" to="/house" style={{margin: "15px"}} >3D House</Link>
    <Link className="nav-list" to="/contact" style={{margin: "15px"}} >Contact</Link>
- </ul>
-
+      </ul>
+      <button  className="closebtn" onClick={CloseNav}
+          >&times;</button>
+        
     </div>
   )
  }
@@ -23,9 +30,9 @@ const Hamb =() =>{
 const Navbar = () => {
 
   const [showNav, setShowNav] = useState(false)
-  const [showB, setShowB] = useState(false)
 
-  const onClick = () => {setShowNav(true); setShowB(true)}
+
+  const onClick = () => {setShowNav(true);}
   const myContainer = useRef(null);
  
 
@@ -47,8 +54,8 @@ const Navbar = () => {
   
    <Breakpoint small down>
    <button onClick={onClick} style={{   backgroundColor: "Transparent", backgroundRepeat:"no-repeat",
-    border: "none", cursor: "pointer", overflow: "hidden", color: "white", 
-    fontSize: "30px", margin:"15px" }}>☰</button>
+        border: "none", cursor: "pointer", overflow: "hidden", color: "white", 
+        fontSize: "30px", margin:"15px" }}>☰</button> 
      { showNav ? <Hamb /> : null }
    
  </Breakpoint>
