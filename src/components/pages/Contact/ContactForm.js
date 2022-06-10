da3a89ce5f62f9f1b4d20b13239db6b72be770b5
Ricardo <rr155914@gmail.com>
2022-06-09 19:27:53 -0500

    Add contact details

--- src/components/pages/Contact/ContactForm.js
+++ src/components/pages/Contact/ContactForm.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import emailjs from "emailjs-com"
+import { Breakpoint } from 'react-socks'
 
 const ContactForm = () => {
 
@@ -11,6 +12,10 @@ const ContactForm = () => {
     }).catch(err => {console.log(err)})
   }
   return (
+    <div>
+    
+    <Breakpoint large up>
+
     <div className="container" style={{width:"40%", marginTop: "120px"}}>
       <h1 style={{ marginTop:"25px", textAlign:"center", fontSize: "35px", }}>Let's talk</h1>
       <form className="row" style={{ margin: "25px 85px 75px 100px"}} onSubmit={sendEmail}>
@@ -23,9 +28,27 @@ const ContactForm = () => {
         <label htmlFor="">Message</label>
         <textarea className="form-control" name="message" rows="4" style={{ backgroundColor: "rgb(255, 208, 208)",width:"90%", marginLeft: "15px"}}/>
         <input className="form-control btn btn-primary" style={{marginTop:"25px", width:"90%", marginLeft: "15px"}} type="submit" value="Send"/>
-
         
       </form>
+      </div>
+    </Breakpoint>
+      <Breakpoint small down>
+      <div className="container" style={{width:"90%", marginTop: "100px"}}>
+      <h1 style={{ marginTop:"25px", textAlign:"center", fontSize: "30px", paddingLeft: "9px"}}>Let's talk</h1>
+      <form className="row" style={{ margin: "25px 85px 75px 90px"}} onSubmit={sendEmail}>
+        <label htmlFor="">Name</label>
+        <input className="form-control" type="text" name= "name"  style={{ backgroundColor: "rgb(255, 208, 208)",width:"90%", marginLeft: "15px"}} />
+
+        <label htmlFor="">Email</label>
+        <input className="form-control" type="email" name= "user_email" style={{ backgroundColor: "rgb(255, 208, 208)",width:"90%", marginLeft: "15px"}} />
+
+        <label htmlFor="">Message</label>
+        <textarea className="form-control" name="message" rows="4" style={{ backgroundColor: "rgb(255, 208, 208)",width:"90%", marginLeft: "15px"}}/>
+        <input className="form-control btn btn-primary" style={{marginTop:"25px", width:"90%", marginLeft: "15px"}} type="submit" value="Send"/>
+        
+      </form>
+      </div>
+      </Breakpoint>
     </div>
   )
 }
