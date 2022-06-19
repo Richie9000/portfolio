15eee2ed2ea8da72d1ece82c2f958e8eae908623
Ricardo <rr155914@gmail.com>
2022-06-18 19:18:39 -0500

    Navbar details

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -23,11 +23,11 @@ const Navbar = () => {
      return(
        <div ref={closed} className="containerS" style={{ top: 0, height: "100%", width: "100%", backgroundColor:"black", zIndex: "2", position: "absolute", opacity: "95%"}}>
          <ul className="list-group list-group-flush" style={{margin: "15px"}}  >
-      <Link className="list-group-item" to="/" style={{margin: "15px", backgroundColor: "Transparent", fontSize:"19px", color: "white", opacity: "25%"}}  >Home</Link>
-      <Link className="list-group-item" to="/watch" style={{  margin: "15px", backgroundColor: "Transparent", fontSize:"19px", color: "white", opacity: "25%"}} >Watch</Link>
-      <Link className="list-group-item" to="/model" style={{margin: "15px", backgroundColor: "Transparent", fontSize:"19px", color: "white", opacity: "25%"}} >Shoe Model</Link>
-      <Link className="list-group-item" to="/house" style={{margin: "15px", backgroundColor: "Transparent", fontSize:"19px", color: "white", opacity: "25%"}} >3D House</Link>
-      <Link className="list-group-item" to="/contact" style={{margin: "15px", backgroundColor: "Transparent", fontSize:"19px", color: "white", opacity: "25%"}} >Contact</Link>
+      <Link className="list-group-item" onClick= {() => { setShowNav(false); CloseNav()}} to="/" style={{margin: "15px", backgroundColor: "Transparent", fontSize:"19px", color: "white", opacity: "25%"}}  >Home</Link>
+      <Link className="list-group-item" to="/watch"  onClick= {() => {setShowNav(false); CloseNav()}} style={{  margin: "15px", backgroundColor: "Transparent", fontSize:"19px", color: "white", opacity: "25%"}} >Watch</Link>
+      <Link className="list-group-item" to="/model"  onClick= {() => {setShowNav(false); CloseNav()}} style={{margin: "15px", backgroundColor: "Transparent", fontSize:"19px", color: "white", opacity: "25%"}} >Shoe Model</Link>
+      <Link className="list-group-item" to="/house"  onClick= {() => {setShowNav(false); CloseNav()}} style={{margin: "15px", backgroundColor: "Transparent", fontSize:"19px", color: "white", opacity: "25%"}} >3D House</Link>
+      <Link className="list-group-item" to="/contact" onClick= {() => {setShowNav(false); CloseNav()}} style={{margin: "15px", backgroundColor: "Transparent", fontSize:"19px", color: "white", opacity: "25%"}} >Contact</Link>
          </ul>
          <button  className="closebtn" onClick= {() => {setShowNav(false); CloseNav()}} style={{   backgroundColor: "Transparent", backgroundRepeat:"no-repeat",
         border: "none", cursor: "pointer", overflow: "hidden", color: "white", top: 0, position:"absolute", right: 2,
--- src/components/pages/Contact/ContactMarkup.js
+++ src/components/pages/Contact/ContactMarkup.js
@@ -8,7 +8,7 @@ const ContactMarkup = () => {
   return (
     <div className="Container" >
       <ContactForm />  
-      <Canvas camera={{ position: [0, 0, 1] }} style={{  position: 'absolute', top: 0, left: 0, pointerEvents: 'none', width: '100%', height: '100%'  } }>
+      <Canvas camera={{ position: [0, 0, 1] }} style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none', width: '100%', height: '100%'  } }>
         <Stars />
       </Canvas>
         </div>
