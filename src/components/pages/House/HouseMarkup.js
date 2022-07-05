c38d640f6849c4d3d7d14a25dd81209da42ebecc
Ricardo <rr155914@gmail.com>
2022-07-05 11:36:44 -0500

    Styling houseMArkup

--- src/components/pages/House/HouseMarkup.js
+++ src/components/pages/House/HouseMarkup.js
@@ -7,15 +7,15 @@ import { Breakpoint } from 'react-socks';
 
 const HouseMarkup = () => {
   return (
-    <div style={{width: "100%", height:"100vh"}}>
+    <div style={{width: "100%", height:"100%"}}>
       <Breakpoint large up >
 
 
         <div style={{display: "flex", justifyContent: "center", margin: "0 auto", alignItems: "center", textAlign:"center"}} >
         <h1 style={{margin:"20px", marginTop: "20px", fontSize:"24px"}}>NFT created for Decentraland Metaverse</h1>
          </div>
-          <div className="containerhouse" style={{ width:"100%", height:"75vh" } }>
-        <Canvas camera={{ fov: 70, position: [ 28, 0, -20 ]}} style={{ width:"100%", height: "70vh" } }>
+          <div className="containerhouse" style={{ display: "flex", justifyContent: "center", width:"100%", height:"72vh" } }>
+        <Canvas camera={{ fov: 70, position: [ 28, 0, -20 ]}} style={{ width:"90%", height: "63vh" } }>
         <Suspense fallback={null}>
        <ambientLight intensity={10}/>
        <spotLight position={[70, 41, 60]} intensity={19} angle={0.29} penumbra={1} shadow-mapSize={[512, 512]} castShadow />
@@ -26,15 +26,15 @@ const HouseMarkup = () => {
 
      </Canvas>
      
+      <footer style={{ margin:"20px",fontSize: '1.2em', fontWeight: 500, letterSpacing: '-0.05em', lineHeight: "1.5em" }}>
+        If you wanna see the live action of the scene, you can visit...
+        <a style={{ margin: "10 px", padding: 0, fontSize: '1.2em', fontWeight: 500, letterSpacing: '-0.05em', lineHeight: "1.9em" }} href="https://housedecentraland.herokuapp.com?realm=localhost-stub&renderer-branch=master">3d World!</a>
+        <div>
+          (Loading the 3d world may take a few moments) If you wanna know more about Decentraland visit....
+        <a style={{ margin: "10 px", padding: 0, fontSize: '1.2em', fontWeight: 500, letterSpacing: '-0.05em', lineHeight: "1.9em" }} href="https://decentraland.org/">Decentraland</a>
+        </div>
+      </footer>
     </div>
-    <footer style={{ margin: "10 px", padding: "20px", fontSize: '1.2em', fontWeight: 500, letterSpacing: '-0.05em', lineHeight: "1.9em" }}>
-      If you wanna see the live action of the scene, you can visit...
-      <a style={{ margin: "10 px", padding: 0, fontSize: '1.2em', fontWeight: 500, letterSpacing: '-0.05em', lineHeight: "1.9em" }} href="https://housedecentraland.herokuapp.com?realm=localhost-stub&renderer-branch=master">3d World!</a>
-      <div>
-        (Loading the 3d world may take a few moments) If you wanna know more about Decentraland visit....
-      <a style={{ margin: "10 px", padding: 0, fontSize: '1.2em', fontWeight: 500, letterSpacing: '-0.05em', lineHeight: "1.9em" }} href="https://decentraland.org/">Decentraland</a>
-      </div>
-    </footer>
      
       </Breakpoint>
       <Breakpoint small down>
