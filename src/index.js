9be345b617e291ee46fa884e7ea2ad6e9c03782e
Ricardo <rr155914@gmail.com>
2023-03-30 19:42:40 -0600

    change font

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
-import { createRoot } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 createRoot(document.getElementById('root')).render(<App />);
